19eea06e94975425388aa3f0291e6d0dc572a8fd
Oleksandr Haleta <alexhaleta99@gmail.com>
2021-11-09 10:03:05 +0200

    fix: bugfix

--- src/main/java/app/model/IExecutorService.java
+++ src/main/java/app/model/IExecutorService.java
@@ -1,45 +1,15 @@
 package app.model;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.*;
 
-/**
- * @author Oleksandr Haleta
- * 2021
- */
-public interface IExecutorService extends ExecutorService {
+public interface IExecutorService {
+    void shutdown();
 
+    List<Runnable> shutdownNow();
 
-    default boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
+    boolean isShutdown();
 
-    default <T> Future<T> submit(Callable<T> task) {
-        return null;
-    }
+    boolean isTerminated();
 
-    default  <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        return null;
-    }
-
-    default <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-        return null;
-    }
-
-    default <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return null;
-    }
-
-    default <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
-    }
-
-    default <T> Future<T> submit(Runnable task, T result) {
-        return null;
-    }
-
-    default Future<?> submit(Runnable task) {
-        return null;
-    }
+    void execute(Runnable command);
 }
